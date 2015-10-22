474b0e047da31a99c42fde88b8db866a90158faa
Colin Hoernig <Hoernig.Colin@gmail.com>
2015-10-22 00:14:40 -0500

    Setup webpack initial config with css/style loaders, hot module replacement, a dev server, and bundle file.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  entry: APP_PATH,
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: APP_PATH
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'KanbanApp'
+    })
+  ]
+};
