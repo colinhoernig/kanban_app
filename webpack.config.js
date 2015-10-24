1bf558ed9175a5f1ce89f7064795b0ceeee790a4
Colin Hoernig <Hoernig.Colin@gmail.com>
2015-10-23 20:23:51 -0400

    Use eval-source-map for source mapping.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
-module.exports = {
+var common = {
   entry: APP_PATH,
   output: {
     path: BUILD_PATH,
@@ -34,3 +36,18 @@ module.exports = {
     })
   ]
 };
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  })
+}
